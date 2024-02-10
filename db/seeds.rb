7169be63e44f9a868d1ad79b51e156e5fdf92cf1
Conan Duval-Manahan <conanduval-manahan@outlook.com>
2024-02-10 17:59:49 +0000

    Blog Post Seeds Added, Confirmation Message for Successful Seeding
    Added Blog post titles and bodies to the database seeds file for
    expanding upon the database's influence on the application with seeds.
    
    Generic blog programming titles used for the blog titles, while lorem
    ipsum has been used for the blog bodies, making them longer in length
    than local development blog posts I have created.
    
    Also added a confirmation message to confirm the seeds have been successfully
    added.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,28 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Sample blog titles
+blog_titles = [
+  "Lorem Ipsum: The Art of Placeholder Text",
+  "10 Tips for Effective Blog Writing",
+  "Why Cats Rule the Internet",
+  "Exploring the Mysteries of Dark Matter",
+  "The Joy of Coding: A Developer's Journey"
+]
+
+# Lorem Ipsum used for the blog bodies
+blog_bodies = [
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+  "Pellentesque habitant morbi tristique senectus et netus...",
+  "Nullam ac urna eu felis dapibus condimentum sit amet...",
+  "Suspendisse potenti. Sed vel libero nec mauris bibendum...",
+  "Fusce dapibus, tellus ac cursus commodo, tortor mauris..."
+]
+
+# This creates the blog posts
+blog_titles.each_with_index do |title, index|
+  Post.create!(title: title, body: blog_bodies[index])
+end
+
+puts "Blog posts have been succesffuly added to the index page!"
