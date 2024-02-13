bbd76148ff819c81221544530e5dbfa27c6f2427
Conan Duval-Manahan <conanduval-manahan@outlook.com>
2024-02-13 19:18:48 +0000

    pg_search gem and Search Functionality Setup
    Implemented pg-search gem, within the Postframework.
    
    Chosen to go with a search scope as opposed to the multi-search option that pg-search also caters for .
    
    Rendered the search view functionality for the pg-search inside a new search form shared view partial. 
    
    Also created a search results partial to handle rendering results dependent upon the entered search parameters, post title specific.
    
    Created post search path route to handle the routing setup of the search functionality for searching for posts.
    
    Tested in console, command "Post.search_by_title("Lorem")" returns one of the database seeds which contains the phrase "Lorem" in it.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
+  include PgSearch::Model
+
+  pg_search_scope :search_by_title, against: :title
+  pg_search_scope :search_by_body, against: :body
+
   validates :title, presence: true
   validates :body, presence: true
 end
